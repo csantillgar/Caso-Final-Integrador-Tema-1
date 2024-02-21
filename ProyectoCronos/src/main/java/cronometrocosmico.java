7cdc51fadf9b030ab71eeccda285d5591efa43e6
csantillgar <csantill@myuax.com>
2024-02-21 12:11:46 +0100

    guardar

--- ProyectoCronos/src/main/java/cronometrocosmico.java
+++ ProyectoCronos/src/main/java/cronometrocosmico.java
@@ -2,7 +2,9 @@ public class cronometrocosmico {
 //Definimos las constantes para los factores de conversión
     private static final double TIERRA_DIAS_ANUALES = 365.25;
     private static final double CRONOS_DIAS_ANUALES = 687;//Dato inventado dias anuales de cronos.
-
+public static double convertirTiempo(double tiempoTierra){
+    return tiempoTierra * (CRONOS_DIAS_ANUALES / TIERRA_DIAS_ANUALES);
+}
 
 
 
