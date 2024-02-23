7b073dccbc4f2c5b953e46ca5d60958de99817d5
csantillgar <csantill@myuax.com>
2024-02-23 14:12:27 +0100

    guardar

--- ProyectoCronos/src/test/java/recursosysuministrostest.java
+++ ProyectoCronos/src/test/java/recursosysuministrostest.java
@@ -1,2 +1,21 @@
-public class recursosysuministrostest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class recursosysuministrostest{
+
+    @Test
+
+    public void calcularConsumoTotal_CalculoCorrecto(){
+        recurso agua = new recurso("Agua", 100);
+        recurso comida = new recurso("Comida", 50);
+        recurso oxigeno = new recurso("Oxigeno", 200);
+
+        recursosysuministros recursosysuministros = new recursosysuministros();
+        recursosysuministros.agregarRecurso(agua);
+        recursosysuministros.agregarRecurso(comida);
+        recursosysuministros.agregarRecurso(oxigeno);
+
+        assertEquals(350, recursosysuministros.calcularConsumoTotal(),0.01);
+    }
+
 }
