2e832a1f5c58e7ca8e04e9811e2ba50986e35f41
csantillgar <csantill@myuax.com>
2024-02-23 10:30:59 +0100

    guardar

--- ProyectoCronos/src/main/java/recursosysuministros.java
+++ ProyectoCronos/src/main/java/recursosysuministros.java
@@ -1,8 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+
+public class recurso {
+    private String nombre;
+    private double cantidad;
+
+    public recurso(String nombre, double cantidad){
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+}
+
+
+
 public class recursosysuministros {
     private List<recurso> recursos;
     public recursosysuministros(){
         this.recursos = new ArrayList<>();
     }
+
 }
