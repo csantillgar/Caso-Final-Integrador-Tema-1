4c39ef8bc3203d611fd76c979148eb3b41a98865
csantillgar <csantill@myuax.com>
2024-02-24 16:35:43 +0100

    guardar

--- ProyectoCronos/src/main/java/sistemaalertamonitoreo.java
+++ ProyectoCronos/src/main/java/sistemaalertamonitoreo.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class sistemaalertamonitoreo {
+    private List<String> tiposEventos;
+    private List<Double> valoresEventos;
+
+    public sistemaalertamonitoreo(){
+        this.tiposEventos = new ArrayList<>();
+        this.valoresEventos = new ArrayList<>();
+    }
 }
 
 
