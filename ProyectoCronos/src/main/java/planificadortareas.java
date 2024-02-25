dd5763058e362bff885a83416420bbc1816872ae
csantillgar <csantill@myuax.com>
2024-02-25 13:03:07 +0100

    guardar

--- ProyectoCronos/src/main/java/planificadortareas.java
+++ ProyectoCronos/src/main/java/planificadortareas.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 public class planificadortareas {
+    private List<Tripulante> tripulacion;
+    public planificadortareas(){
+        this.tripulacion = new ArrayList<>();
+    }
+    public void agregarTripulante(Tripulante tripulante){
+        tripulacion.add(tripulante);
+    }
+
+
+
 }
