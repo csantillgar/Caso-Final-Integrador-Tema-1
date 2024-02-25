acc56e3d8e0a5cdfa151eccc5a952219aec15d74
csantillgar <csantill@myuax.com>
2024-02-25 18:02:53 +0100

    guardar

--- ProyectoCronos/src/main/java/VI_comunicadorinterplanetario.java
+++ ProyectoCronos/src/main/java/VI_comunicadorinterplanetario.java
@@ -13,8 +13,16 @@ public class VI_comunicadorinterplanetario {
     public static String invertirMensaje(String mensaje) {
         return new StringBuilder(mensaje).reverse().toString();
     }
-
-
-
+    //verifica si un mensaje es un palíndromo
+    public static boolean esPalindromo(String mensaje) {
+        String clean = mensaje.replaceAll("\\s+", "").toLowerCase();
+        int length = clean.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (clean.charAt(i) != clean.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
