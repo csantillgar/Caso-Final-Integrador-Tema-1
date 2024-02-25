57ae48a22bb9c6e279b10068670d615b74931113
csantillgar <csantill@myuax.com>
2024-02-25 17:53:42 +0100

    guardar

--- ProyectoCronos/src/main/java/VI_comunicadorinterplanetario.java
+++ ProyectoCronos/src/main/java/VI_comunicadorinterplanetario.java
@@ -1,2 +1,14 @@
 public class VI_comunicadorinterplanetario {
+    public static int contarVocales(String mensaje) {
+        int count = 0;
+        for (char c : mensaje.toLowerCase().toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
 }
