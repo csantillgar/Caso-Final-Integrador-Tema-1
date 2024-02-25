f02b22c97e849fae4501b505fc4d26d74180fba7
csantillgar <csantill@myuax.com>
2024-02-25 17:31:40 +0100

    guardar

--- ProyectoCronos/src/test/java/V_navegadorestelartest.java
+++ ProyectoCronos/src/test/java/V_navegadorestelartest.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class V_navegadorestelartest {
+
+    @Test
+    public void generarTerrenoAleatorio() {
+        V_navegadorestelar navegador = new V_navegadorestelar(5, 5);
+        navegador.generarTerrenoAleatorio();
+
+        // Verificar que el tamaño del terreno sea correcto
+        assertEquals(5, navegador.getTerreno().length);
+        assertEquals(5, navegador.getTerreno()[0].length);
+    }
+
+
 }
