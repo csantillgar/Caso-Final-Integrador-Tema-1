a715ed3ebfaf76921cee317bfdfa9ebdd1271cf7
csantillgar <csantill@myuax.com>
2024-02-25 12:42:51 +0100

    guardar

--- ProyectoCronos/src/test/java/sistemaalertamonitoreotest.java
+++ ProyectoCronos/src/test/java/sistemaalertamonitoreotest.java
@@ -1,2 +1,24 @@
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class sistemaalertamonitoreotest {
-}
+
+    @Test
+    public void monitorear_DeteccionEventosRaros() {
+        sistemaalertamonitoreo sistemaalertamonitoreo = new sistemaalertamonitoreo();
+
+        sistemaalertamonitoreo.monitorear("Temperatura", 100);
+        sistemaalertamonitoreo.monitorear("Temperatura", 80);
+        sistemaalertamonitoreo.monitorear("Temperatura", 110);
+        sistemaalertamonitoreo.monitorear("Temperatura", 60);
+
+        List<String> eventosRaros = sistemaalertamonitoreo.obtenerEventosRaros(3);
+
+        assertEquals(3, eventosRaros.size());
+        assertTrue(eventosRaros.contains("Temperatura alta"));
+    }
+
+    }
+
