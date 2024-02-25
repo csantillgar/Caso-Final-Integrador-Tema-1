e4b69c91f666e63122603c72c9d23eb9fe09c338
csantillgar <csantill@myuax.com>
2024-02-25 13:08:58 +0100

    guardar

--- ProyectoCronos/src/main/java/planificadortareas.java
+++ ProyectoCronos/src/main/java/planificadortareas.java
@@ -8,7 +8,15 @@ public class planificadortareas {
     public void agregarTripulante(Tripulante tripulante){
         tripulacion.add(tripulante);
     }
-
+    public void distribuirTareas(List<Integer> tareas){
+        if (tareas.size() != tripulacion.size()){
+            throw new IllegalArgumentException("La cantidad de tareas debe coincidir con la cantidad de tripulantes");
+        }
+        for (int i = 0; i < tripulacion.size(); i++){
+            tripulacion.get(i).agregarTarea(tareas.get(i));
+        }
+    }
+    public void visualizarHorarios
 
 
 }
