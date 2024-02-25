f9de03769c7f339dabf942138677d540d3d7ed03
csantillgar <csantill@myuax.com>
2024-02-25 18:05:33 +0100

    guardar

--- ProyectoCronos/src/test/java/VI_comunicadorinterplanetariotest.java
+++ ProyectoCronos/src/test/java/VI_comunicadorinterplanetariotest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 public class VI_comunicadorinterplanetariotest {
+    @Test
+    public void testContarVocales() {
+        assertEquals(3, VI_comunicadorinterplanetario.contarVocales("Hello World"));
+    }
+
+
 }
