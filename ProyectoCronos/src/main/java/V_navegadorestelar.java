704e43609a8138070227b7df60b672ad4671e7be
csantillgar <csantill@myuax.com>
2024-02-25 17:24:50 +0100

    guardar

--- ProyectoCronos/src/main/java/V_navegadorestelar.java
+++ ProyectoCronos/src/main/java/V_navegadorestelar.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
 public class V_navegadorestelar {
+    private int[][] terreno;
+
+    public V_navegadorestelar(int filas, int columnas) {
+        this.terreno = new int[filas][columnas];
+    }
+    public void generarTerrenoAleatorio() {
+        for (int i = 0; i < terreno.length; i++) {
+            for (int j = 0; j < terreno[0].length; j++) {
+                terreno[i][j] = (int) (Math.random() * 10);
+            }
+        }
+    }
+
+    public void visualizarTerreno() {
+        for (int[] fila : terreno) {
+            System.out.println(Arrays.toString(fila));
+        }
+    }
 }
